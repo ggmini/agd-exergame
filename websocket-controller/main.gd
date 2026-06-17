34cae731c09d32e8c53520c48d99e053a05ff335
ggmini <minigsm14@gmail.com>
2026-06-17 16:24:45 +0200

    websocket project added

---
+++ websocket-controller/main.gd
@@ -0,0 +1,46 @@
+extends Node3D
+
+
+@export var web_socket_client: Node
+
+var pitch: float = 0.0
+var roll: float = 0.0
+var yaw: float = 0.0
+
+var initial_yaw : float = 0.0
+
+var k : float = 0.98
+
+func _ready():
+	await get_tree().process_frame
+	var magnet: Vector3 = Input.get_magnetometer()
+	#print(magnet)
+	initial_yaw = atan2(-magnet.x, magnet.z) 
+
+func _physics_process(delta):
+	var magnet: Vector3 = Input.get_magnetometer().rotated(-Vector3.FORWARD, rotation.z).rotated(Vector3.RIGHT, rotation.x)
+	var gravity: Vector3 = Input.get_gravity()
+	var roll_acc = atan2(-gravity.x, -gravity.y) 
+	gravity = gravity.rotated(-Vector3.FORWARD, rotation.z)
+	var pitch_acc = atan2(gravity.z, -gravity.y)
+	var yaw_magnet = atan2(-magnet.x, magnet.z)
+	
+	var gyroscope: Vector3 = Input.get_gyroscope().rotated(-Vector3.FORWARD, roll)
+	pitch = lerp_angle(pitch_acc, pitch + gyroscope.x * delta, k)
+	yaw = lerp_angle(yaw_magnet, yaw + gyroscope.y * delta, k)
+	roll = lerp_angle(roll_acc, roll + gyroscope.z * delta, k) 
+	
+	#node.rotation = Vector3(pitch, yaw - initial_yaw, roll)
+	#node.rotation = Vector3(0, yaw - initial_yaw, roll)
+	#node.rotation = Vector3(0, yaw - initial_yaw, 0)
+	#csg_box_3d.rotation = Vector3(0, 0, roll)
+	#roll_label.text = "Roll: " + str(roll)
+	#print(roll)
+	
+	var msg: Dictionary = {
+		"pitch": str(pitch),
+		"yaw": str(yaw),
+		"roll": str(roll)
+	}
+	web_socket_client.msg = msg
+	
---
+++ websocket-controller/web_socket_client.gd
@@ -0,0 +1,75 @@
+extends Node
+
+# The URL we will connect to.
+# Use "ws://localhost:9080" if testing with the minimal server example below.
+# `wss://` is used for secure connections,
+# while `ws://` is used for plain text (insecure) connections.
+@export var websocket_url = "ws://192.168.0.12:9080"
+#@export var websocket_url = "ws://88.209.32.97"
+#@export var websocket_url = "ws://localhost:9080"
+@export var text_edit: TextEdit
+
+# Our WebSocketClient instance.
+var socket = WebSocketPeer.new()
+
+var msg: Dictionary = {}
+
+
+func _ready():
+	pass
+
+
+func attempt_connection():
+	var address: String = "ws://" + text_edit.text + ":9080"
+	# Initiate connection to the given URL.
+	var err = socket.connect_to_url(address)
+	if err == OK:
+		print("Connecting to %s..." % address)
+		# Wait for the socket to connect.
+		await get_tree().create_timer(2).timeout
+
+		# Send data.
+		print("> Sending test packet.")
+		socket.send_text("Test packet")
+	else:
+		push_error("Unable to connect.")
+		set_process(false)
+
+
+func _physics_process(delta: float) -> void:
+	# Call this in `_process()` or `_physics_process()`.
+	# Data transfer and state updates will only happen when calling this function.
+	socket.poll()
+
+	# get_ready_state() tells you what state the socket is in.
+	var state = socket.get_ready_state()
+
+	# `WebSocketPeer.STATE_OPEN` means the socket is connected and ready
+	# to send and receive data.
+	if state == WebSocketPeer.STATE_OPEN:
+		#while socket.get_available_packet_count():
+			#var packet = socket.get_packet()
+			#if socket.was_string_packet():
+				#var packet_text = packet.get_string_from_utf8()
+				#print("< Got text data from server: %s" % packet_text)
+			#else:
+				#print("< Got binary data from server: %d bytes" % packet.size())
+		if !msg.is_empty():
+			socket.send_text(str(msg))
+		
+	# `WebSocketPeer.STATE_CLOSING` means the socket is closing.
+	# It is important to keep polling for a clean close.
+	elif state == WebSocketPeer.STATE_CLOSING:
+		pass
+
+	# `WebSocketPeer.STATE_CLOSED` means the connection has fully closed.
+	# It is now safe to stop polling.
+	elif state == WebSocketPeer.STATE_CLOSED:
+		# The code will be `-1` if the disconnection was not properly notified by the remote peer.
+		var code = socket.get_close_code()
+		print("WebSocket closed with code: %d. Clean: %s" % [code, code != -1])
+		set_process(false) # Stop processing.
+
+
+func _on_button_pressed() -> void:
+	attempt_connection()
