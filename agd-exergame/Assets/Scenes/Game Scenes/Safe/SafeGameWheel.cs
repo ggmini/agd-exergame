a8cc4235f8016bb89a9224e68d7a7be241e76a95
enr.a <enra94@gmail.com>
2026-07-01 16:51:00 +0200

    update(safe): added game

---
+++ agd-exergame/Assets/Scenes/Game Scenes/Safe/SafeGameWheel.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class SafeGameWheel : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (WebSocketManager.Instance.Msg == null) return;
+
+        Vector3 rot = new Vector3(0.0f, 0.0f, WebSocketManager.Instance.Msg.roll * Mathf.Rad2Deg);
+        transform.eulerAngles = new Vector3(0.0f, 0.0f, WebSocketManager.Instance.Msg.roll);
+    }
+}
