9e226ed6560ad8fbea9c1c170b6e1aa135202ea8
ggmini <minigsm14@gmail.com>
2026-07-15 21:12:29 +0200

    feat(wiimote): added variable threshold, max speed & reset for accelorometer

--- agd-exergame/Assets/Scripts/Wiimote Input/Wiimote Tester.cs
+++ agd-exergame/Assets/Scripts/Wiimote Input/Wiimote Tester.cs
@@ -13,7 +13,15 @@ public class WiimoteTester : MonoBehaviour {
 	[Range(0f, 5f)]
 	float accelSensitivity = 1f;
 
-    Rigidbody rb;
+    [SerializeField]
+    [Range(0f, 5f)]
+    float maxSpeed = 1f;
+
+	[SerializeField]
+    [Range(0f, 1f)]
+    float threshold = 0.2f;
+
+	Rigidbody rb;
 
     void Start() {
         rb = GetComponent<Rigidbody>();
@@ -24,9 +32,15 @@ public class WiimoteTester : MonoBehaviour {
         transform.localRotation = input.Rotation;
         if (readAccel) {
             Vector3 vel = rb.linearVelocity;
-            vel.x = input.GetAccelX() * accelSensitivity * accelSensitivity;
-			rb.linearVelocity = vel;
-		}
+            float x = input.GetAccelX();
+            if (x > threshold || x < -threshold) {
+                vel.x += x * accelSensitivity * accelSensitivity;
+                vel.x = Mathf.Clamp(vel.x, -maxSpeed, maxSpeed);
+                Debug.Log(vel.x);
+                rb.linearVelocity = vel;
+            } else
+                rb.linearVelocity = Vector3.zero;
+        }
     }
 
 }
